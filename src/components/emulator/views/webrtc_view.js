951226367b2b2d447542ffb48a2c0f4ba282eebd
Erwin Jansen <jansene@google.com>
2020-07-24 12:00:45 -0700

    Do not assume video object returns a promise
    Not every browser returns a promise when calling the play
    method on a video element. See
    https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/play

--- src/components/emulator/views/webrtc_view.js
+++ src/components/emulator/views/webrtc_view.js
@@ -44,7 +44,8 @@ export default class EmulatorWebrtcView extends Component {
     muted: false,
     volume: 1.0,
     onError: (e) => console.error("WebRTC error: " + e),
-    onAudioStateChange: (e) => console.log("Webrtc audio became available: " + e),
+    onAudioStateChange: (e) =>
+      console.log("Webrtc audio became available: " + e),
   };
 
   constructor(props) {
@@ -61,18 +62,23 @@ export default class EmulatorWebrtcView extends Component {
 
   componentWillUnmount() {
     this.props.jsep.disconnect();
+    this.setState;
   }
 
   componentDidMount() {
     this.props.jsep.on("connected", this.onConnect);
     this.props.jsep.on("disconnected", this.onDisconnect);
-    this.setState({ connect: "connecting" }, this.broadcastState);
-    this.props.jsep.startStream();
+    this.setState({ connect: "connecting" }, () => {
+      this.props.jsep.startStream();
+      this.broadcastState();
+    });
   }
 
   onDisconnect = () => {
     this.setState({ connect: "disconnected" }, this.broadcastState);
-    this.setState({ audio: false }, this.props.onAudioStateChange(false));
+    this.setState({ audio: false }, () => {
+      this.props.onAudioStateChange(false);
+    });
   };
 
   onConnect = (track) => {
@@ -88,7 +94,9 @@ export default class EmulatorWebrtcView extends Component {
     }
     video.srcObject.addTrack(track);
     if (track.kind === "audio") {
-      this.setState({ audio: true }, this.props.onAudioStateChange(true));
+      this.setState({ audio: true }, () => {
+        this.props.onAudioStateChange(true);
+      });
     }
   };
 
@@ -101,15 +109,18 @@ export default class EmulatorWebrtcView extends Component {
       return;
     }
 
-    video
-      .play()
-      .then((_) => {
-        console.info("Automatic playback started!");
-      })
-      .catch((error) => {
-        // Notify listeners that we cannot start.
-        this.onError(error);
-      });
+    // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/play
+    const possiblePromise = video.play();
+    if (possiblePromise) {
+      possiblePromise
+        .then((_) => {
+          console.info("Automatic playback started!");
+        })
+        .catch((error) => {
+          // Notify listeners that we cannot start.
+          this.onError(error);
+        });
+    }
   };
 
   onCanPlay = (e) => {
